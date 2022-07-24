8164962b902431d26676cb2528c72927fd749369
borealism <tuncalburak@gmail.com>
2022-07-24 14:07:07 +0300

    true/false islemleri

---
+++ 13-Boolean/project.js
@@ -0,0 +1,31 @@
+// console.log(true, false, 'true', 'false');
+
+//Include kullanımı
+
+// let email = "yalperenbozkurt@gmail.com";
+//     let varmi = email.includes('x');
+//     console.log(varmi);
+
+
+// Operatörler
+
+let yas = 31; 
+
+// console.log(yas == 31);
+// console.log(yas == 35);
+// console.log(yas != 25);
+// console.log(yas > 31);
+// console.log(yas < 31);
+// console.log(yas >= 31);
+// console.log(yas <= 31);
+
+
+
+// String eşitliği 
+
+let ad = 'can';
+console.log(ad == 'can');
+console.log(ad == 'Can');
+console.log(ad > 'Can');
+
+
