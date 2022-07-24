16604201128066e5936d4c39e9aba9f8919f5be0
borealism <tuncalburak@gmail.com>
2022-07-24 14:13:44 +0300

    typeOf islemleri

---
+++ 14-TypeveKarsilastirma/project.js
@@ -0,0 +1,18 @@
+let yas = "31";
+console.log(yas + 1);
+
+console.log(typeof yas);
+
+// yas = Number(yas);
+// console.log(yas + 1);
+// console.log(typeof yas);
+
+// == vs. ===
+
+console.log(yas == 31);
+console.log(yas == '31');
+
+console.log(yas === 31);
+console.log(yas === '31');
+
+
